0597bba97e958abd4b9c39fbee18dc1f1c7886b8
marjan <marjan199721@gmail.com>
2024-06-08 21:57:19 +0200

    get, delete and update user data

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -69,9 +69,112 @@ const logoutUser = errorHandler(async (req, res) => {
 
 // get all users
 
-const getAllUsers = errorHandler(async(req,res)=>{
-const users = await User.find({});
-res.json(users)
-})
+const getAllUsers = errorHandler(async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+});
 
-export { createUser, loginUser, logoutUser , getAllUsers};
+// get single user
+
+const getCurrentUser = errorHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found.");
+  }
+});
+
+// update user
+const updateUserProfile = errorHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    user.username = req.body.username || user.username;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      const salt = await bcrypt.genSalt(10);
+
+      const hashPassword = await bcrypt.hash(req.body.password, salit);
+      user.password = hashPassword;
+    }
+
+    const updateUser = await user.save();
+
+    res.json({
+      _id: updateUser._id,
+      username: updateUser.username,
+      email: updateUser.email,
+      isAdmin: updateUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+// admin can delete user
+const deleteUser = errorHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    if (user.isAdmin) {
+      res.status(400);
+      throw new Error("Cannot delete admin");
+    }
+
+    await user.deleteOne({ _id: user._id });
+    res.json({ message: "User removed Successfully" });
+  } else {
+    res.status(404);
+    throw new Error("User not found ");
+  }
+});
+
+const getUserById = errorHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select("-passsword");
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+const updateUserById = errorHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (user) {
+    user.username = req.body.username || user.username;
+    user.email = req.body.email || user.email;
+    user.isAdmin = Boolean(req.body.isAdmin);
+
+    const updateUser = await user.save();
+    res.json({
+      _id: updateUser._id,
+      username: updateUser._username,
+      email: updateUser.email,
+      isAdmin: updateUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+export {
+  createUser,
+  loginUser,
+  logoutUser,
+  getAllUsers,
+  getCurrentUser,
+  updateUserProfile,
+  deleteUser,
+  getUserById,
+  updateUserById,
+};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 
 import express from 'express'
-import { createUser,loginUser,logoutUser, getAllUsers } from '../controllers/userControllers.js';
+import { createUser,loginUser,logoutUser, getAllUsers, getCurrentUser, updateUserProfile, deleteUser,getUserById,updateUserById } from '../controllers/userControllers.js';
 import { adminCheck, authenticate } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
@@ -8,6 +8,8 @@ const router = express.Router();
 router.route('/').post(createUser).get(authenticate,adminCheck,getAllUsers);
 router.post('/auth',loginUser);
 router.post('/logout', logoutUser)
+router.route('/profile').get(authenticate,getCurrentUser).put(authenticate,updateUserProfile)
+// admin functionality
 
-
+router.route('/:id').delete(authenticate,adminCheck,deleteUser).get(authenticate,adminCheck,getUserById).put(authenticate,adminCheck,updateUserById)
 export default router;
